84c758ecb2050d1de60422b80125caee121c6071
Nethetla Raju <nnraju2003@gmail.com>
2025-12-10 11:18:25 +0530

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showShop() {
+    document.getElementById("home").classList.add("hidden");
+    document.getElementById("shop").classList.remove("hidden");
+}
+
+function showHome() {
+    document.getElementById("shop").classList.add("hidden");
+    document.getElementById("home").classList.remove("hidden");
+}
